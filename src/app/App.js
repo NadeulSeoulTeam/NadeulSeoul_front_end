8007e54504dcded74073a77ebfbb7f3b83b5af9c
yoon <jiyoonhan12@gmail.com>
2022-01-20 14:29:28 +0900

    Add : google social login 진행중

--- src/app/App.js
+++ src/app/App.js
@@ -5,7 +5,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../features/test_todo/Home';
 import Detail from '../features/test_todo/Detail';
 import NaverLogin from '../features/Auth/Naver/NaverLogin';
+import GoogleLogin from '../features/Auth/Google/GoogleLogin';
 import NaverLoginCallback from '../features/Auth/Naver/NaverLoginCallback';
+import GoogleLoginCallback from '../features/Auth/Google/GoogleLoginCallback';
 
 // Common
 import Error404 from '../common/error/Error404';
@@ -17,7 +19,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Detail />} />
         <Route path="/login" element={<NaverLogin />} />
+        {/* login 파일 통일 필요 */}
+        <Route path="/googlelogin" element={<GoogleLogin />} />
         <Route path="/callback" element={<NaverLoginCallback />} />
+        <Route path="/auth/google/callback" element={<GoogleLoginCallback />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
---
+++ src/features/Auth/Google/GoogleLoginCallback.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function GoogleLoginCallback() {
+  return <>GoogleLoginCallback</>;
+}
+
+export default GoogleLoginCallback;
