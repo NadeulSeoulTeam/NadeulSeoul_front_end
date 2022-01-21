0ad0f0c751ef167bdf03146826b9e219e9cc11f8
yoon <yoonnotyoun@gmail.com>
2022-01-21 23:21:50 +0900

    Update : mainpage mui styled-component style 적용

--- src/features/Main/MainPage.js
+++ src/features/Main/MainPage.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
-import Button from '@mui/material/Button';
-import { StylesProvider } from '@material-ui/core';
-
-export const MainTitle = styled.h1`
-  font-family: 'Hahmlet';
-  font-size: 48px;
-  text-align: center;
-  color: #68c78e;
-`;
-
-export const GreenBtn = styled(Button)`
-  font-family: 'Suit';
-  font-weight: bold;
-  background-color: #68c78e;
-  padding: 12px 24px;
-  border-radius: 50px;
-  color: white;
-  cursor: pointer;
-  border: none;
-`;
+import { StylesProvider } from '@material-ui/core/styles';
+import SearchBar from 'src/common/SearchBar/SearchBar';
+import { Wrapper, MainTitle, GreenBtn } from './MainPageStyle';
 
 function MainPage() {
   return (
-    <div>
-      <MainTitle>나들서울</MainTitle>
-      <GreenBtn>현재 위치 기준으로 보기</GreenBtn>
-    </div>
+    <StylesProvider injectFirst>
+      <Wrapper>
+        <MainTitle>나들서울</MainTitle>
+        <SearchBar />
+        <GreenBtn>현재 위치 기준으로 보기</GreenBtn>
+      </Wrapper>
+    </StylesProvider>
   );
 }
 
--- src/features/Main/MainPageStyle.js
+++ src/features/Main/MainPageStyle.js
@@ -1,12 +1,14 @@
-import React from 'react';
 import styled from 'styled-components';
-import Button from '@mui/material/Button';
-import { StylesProvider } from '@mui/styles';
+import Button from '@material-ui/core/Button';
+
+export const Wrapper = styled.div`
+  background-color: #e5e5e5;
+  text-align: center;
+`;
 
 export const MainTitle = styled.h1`
   font-family: 'Hahmlet';
   font-size: 48px;
-  text-align: center;
   color: #68c78e;
 `;
 
@@ -14,17 +16,11 @@ export const GreenBtn = styled(Button)`
   font-family: 'Suit';
   font-weight: bold;
   background-color: #68c78e;
-  padding: 12px 24px;
+  padding: 12px 20px;
   border-radius: 50px;
   color: white;
   cursor: pointer;
   border: none;
 `;
 
-export default function StyledComponents() {
-  return (
-    <StylesProvider injectFirst>
-      <GreenBtn />
-    </StylesProvider>
-  );
-}
+// export default { Wrapper, MainTitle, GreenBtn };
