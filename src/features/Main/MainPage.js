0dd2b0e512c415521c1dc76317703f50be6350be
yoonnotyoun <yoonnotyoun@gmail.com>
2022-01-21 23:31:23 +0900

    Update : error 주석처리

--- src/features/Main/MainPage.js
+++ src/features/Main/MainPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StylesProvider } from '@material-ui/core/styles';
-import SearchBar from 'src/common/SearchBar/SearchBar';
 import { Wrapper, MainTitle, GreenBtn } from './MainPageStyle';
+// import SearchBar from '../common/SearchBar/SearchBar';
 
 function MainPage() {
   return (
     <StylesProvider injectFirst>
       <Wrapper>
         <MainTitle>나들서울</MainTitle>
-        <SearchBar />
+        {/* <SearchBar /> */}
         <GreenBtn>현재 위치 기준으로 보기</GreenBtn>
       </Wrapper>
     </StylesProvider>
