3ed383accd7f413e41749b40247fb5aa8ba06876
yoonnotyoun <yoonnotyoun@gmail.com>
2022-02-03 18:42:58 +0900

    Update : Signin css 추가

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// style
+import GlobalFonts from '../fonts/fonts';
+import Container from './AppStyle';
+
 // features
+import SignIn from '../features/Auth/SignIn';
 import NaverLogin from '../features/Auth/Naver/NaverLogin';
-import GoogleLogin from '../features/Auth/Google/GoogleLogin';
+// import GoogleLogin from '../features/Auth/Google/GoogleLogin';
 import NaverLoginCallback from '../features/Auth/Naver/NaverLoginCallback';
-import GoogleLoginCallback from '../features/Auth/Google/GoogleLoginCallback';
+// import GoogleLoginCallback from '../features/Auth/Google/GoogleLoginCallback';
 import MyPage from '../features/mypage/MyPage';
 
 // Common
@@ -16,19 +21,25 @@ import CourseView from '../features/CourseView/CourseView';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/naverlogin" element={<NaverLogin />} />
-        <Route path="/mypage" element={<MyPage />} />
-        {/* login 파일 통일 필요 */}
-        <Route path="/member/signin" element={<GoogleLogin />} />
-        <Route path="/auth/naver/callback/" element={<NaverLoginCallback />} />
-        <Route path="/auth/google/callback" element={<GoogleLoginCallback />} />
-        <Route path="/Course" element={<Course />} />
-        <Route path="/CourseView" element={<CourseView />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <GlobalFonts />
+        <Routes>
+          <Route path="/naverlogin" element={<NaverLogin />} />
+          <Route path="/mypage" element={<MyPage />} />
+          {/* login 파일 통일 필요 */}
+          <Route path="/member/signin" element={<SignIn />} />
+          <Route
+            path="/auth/naver/callback/"
+            element={<NaverLoginCallback />}
+          />
+          {/* <Route path="/auth/google/callback" element={<GoogleLoginCallback />} /> */}
+          <Route path="/Course" element={<Course />} />
+          <Route path="/CourseView" element={<CourseView />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
---
+++ src/app/AppStyle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  min-width: 100%;
+  min-height: 100vh;
+  margin: 0;
+  background-color: #e5e5e5;
+  font-family: 'Suit';
+`;
+
+export default Container;
