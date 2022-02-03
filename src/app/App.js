e7fe03be5cd60ff06c6a46c5d140441645cf9698
yoonnotyoun <yoonnotyoun@gmail.com>
2022-02-03 13:46:38 +0900

    Update : merge 이후

--- src/app/App.js
+++ src/app/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // features
-import Home from '../features/test_todo/Home';
-import Detail from '../features/test_todo/Detail';
 import NaverLogin from '../features/Auth/Naver/NaverLogin';
 import GoogleLogin from '../features/Auth/Google/GoogleLogin';
 import NaverLoginCallback from '../features/Auth/Naver/NaverLoginCallback';
@@ -12,19 +10,14 @@ import MyPage from '../features/mypage/MyPage';
 
 // Common
 import Error404 from '../common/error/Error404';
-<<<<<<< HEAD
 // import BasicTabs from '../features/mypage/BasicTabs';
-=======
 import Course from '../features/CourseMake/Course';
 import CourseView from '../features/CourseView/CourseView';
->>>>>>> dd219033470a465a43ad423b3143233ebc84baba
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Detail />} />
         <Route path="/naverlogin" element={<NaverLogin />} />
         <Route path="/mypage" element={<MyPage />} />
         {/* login 파일 통일 필요 */}
