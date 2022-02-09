508569ea70ea54494b1e528926fa66f39db0c554
yoonnotyoun <yoonnotyoun@gmail.com>
2022-02-09 14:25:21 +0900

    Update : route 수정

--- src/app/App.js
+++ src/app/App.js
@@ -8,16 +8,11 @@ import Container from './AppStyle';
 // features
 import SignIn from '../features/Auth/SignIn';
 import UserForm from '../features/Auth/UserForm';
-import NaverLogin from '../features/Auth/Naver/NaverLogin';
-// import GoogleLogin from '../features/Auth/Google/GoogleLogin';
-import NaverLoginCallback from '../features/Auth/Naver/NaverLoginCallback';
-// import GoogleLoginCallback from '../features/Auth/Google/GoogleLoginCallback';
 import MyPage from '../features/MyPage';
 import MainPage from '../features/Main/MainPage';
 
 // Common
 import Error404 from '../common/error/Error404';
-// import BasicTabs from '../features/mypage/BasicTabs';
 import Course from '../features/CourseMake/Course';
 import CourseCreationForm from '../features/CourseMake/CourseCreationForm';
 import CourseView from '../features/CourseView';
@@ -33,11 +28,12 @@ function App() {
       <BrowserRouter>
         <GlobalFonts />
         <Routes>
-          <Route Route path="/main" element={<MainPage />} />
+          <Route Route path="/" element={<MainPage />} />
           {/* auth */}
-          <Route path="/naverlogin" element={<NaverLogin />} />
-          <Route Route path="/mypage/:id" element={<MyPage />} />
+          <Route path="/member/signin" element={<SignIn />} />
+          <Route path="/member/signup" element={<UserForm />} />
           {/* mypage */}
+          <Route Route path="/mypage/:id" element={<MyPage />} />
           <Route
             Route
             path="/mypage/:id/follower"
@@ -54,22 +50,7 @@ function App() {
             path="/questions/:QuestionId"
             element={<BoardListItem />}
           />
-          {/* 수정 필요(문의게시판 밖으로) */}
-          {/* <Route
-            Route
-            path="mypage/:id/BoardList/:PostId"
-            element={<BoardListItem />}
-          /> */}
           <Route Route path="/questions/new" element={<BoardForm />} />
-
-          {/* login 파일 통일 필요 */}
-          <Route path="/member/signin" element={<SignIn />} />
-          <Route path="/member/signup" element={<UserForm />} />
-          <Route
-            path="/auth/naver/callback/"
-            element={<NaverLoginCallback />}
-          />
-          {/* <Route path="/auth/google/callback" element={<GoogleLoginCallback />} /> */}
           <Route path="/Course" element={<Course />} />
           <Route path="/CourseView" element={<CourseView />} />
           <Route path="/CourseCreationForm" element={<CourseCreationForm />} />
