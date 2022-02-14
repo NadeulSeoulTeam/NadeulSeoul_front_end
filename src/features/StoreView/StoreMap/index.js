a97e2ca3e6d1bdfa0429c8c599bc6634a774db6b
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-02-14 14:57:58 +0900

    Update : StoreView map marker 수정

--- src/features/StoreView/StoreMap/index.js
+++ src/features/StoreView/StoreMap/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-continue */
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
@@ -8,6 +9,7 @@ import {
   // moveToList,
   getClicked,
   setClicked,
+  getClickedIndex,
 } from '../StoreSlice';
 
 import { addMarker, setMarker, removeMarker } from './StoreMarker/StoreMarker';
@@ -21,6 +23,7 @@ function StoreMap() {
   const level = useSelector(selectLevel);
   const searchData = useSelector(getSearchData);
   const clicked = useSelector(getClicked);
+  const clickedIndex = useSelector(getClickedIndex);
 
   // eslint-disable-next-line no-unused-vars
   const [map, setMap] = useState();
@@ -64,7 +67,7 @@ function StoreMap() {
         searchData.data[i].y,
         searchData.data[i].x
       );
-      tempMarkers.push(addMarker(kakao, map, placePosition, i)); // 배열에 생성된 마커를 추가합니다
+      tempMarkers.push(addMarker(kakao, map, placePosition, i, null)); // 배열에 생성된 마커를 추가합니다
       if (i === 0) {
         map.panTo(placePosition);
         // dispatch(
@@ -75,8 +78,37 @@ function StoreMap() {
     // map.setBounds(bounds);
     setTempMarkers(tempMarkers);
     setMarker(map, tempMarkers);
-    // markerClickEventHandler();
   }, [searchData]);
+  useEffect(() => {
+    // const bounds = new kakao.maps.LatLngBounds(); // bounds 설정
+    // 마커 초기화
+    console.log(clickedIndex, 'clicked!!');
+    removeMarker(tempMarkers);
+    setTempMarkers(tempMarkers.splice(0, tempMarkers.length));
+    // 마커 추가
+    // eslint-disable-next-line no-plusplus
+    let tempPlacePosition;
+    for (let i = 0; i < searchData.data.length; i += 1) {
+      if (i === clickedIndex) {
+        tempPlacePosition = new kakao.maps.LatLng(
+          searchData.data[i].y,
+          searchData.data[i].x
+        );
+        continue;
+      }
+      const placePosition = new kakao.maps.LatLng(
+        searchData.data[i].y,
+        searchData.data[i].x
+      );
+      tempMarkers.push(addMarker(kakao, map, placePosition, i, clickedIndex)); // 배열에 생성된 마커를 추가합니다
+    }
+    tempMarkers.push(
+      addMarker(kakao, map, tempPlacePosition, clickedIndex, clickedIndex)
+    );
+    // map.setBounds(bounds);
+    setTempMarkers(tempMarkers);
+    setMarker(map, tempMarkers);
+  }, [clickedIndex]);
   useEffect(
     () => {
       console.log('lat, lang changed');
