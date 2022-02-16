2f34c89c192527d7448c3ae1810afae88490a1f7
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-02-17 03:24:20 +0900

    Update Course add axios

--- src/common/SearchBar/index.js
+++ src/common/SearchBar/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 import { Wrapper, SearchBase, SearchInput, SearchBtn } from './styles';
 
 function SearchBar() {
+  const [searchInput, setSearchInput] = useState();
+  const navigate = useNavigate();
+
+  const setSearch = (e) => {
+    setSearchInput(e.target.value);
+  };
+  const search = () => {
+    navigate(`/storeview`);
+  };
+  useEffect(() => {
+    console.log(searchInput);
+  }, [searchInput]);
   return (
     <Wrapper container direction="row" justifyContent="center">
       <SearchBase elevation={1}>
-        <SearchInput placeholder="어디로 떠날까요?" />
-        <SearchBtn />
+        <SearchInput onChange={setSearch} placeholder="어디로 떠날까요?" />
+        <SearchBtn onClick={search} />
       </SearchBase>
     </Wrapper>
   );
--- src/features/CourseMake/CourseCreationForm/index.js
+++ src/features/CourseMake/CourseCreationForm/index.js
@@ -259,47 +259,47 @@ function CourseCreactionForm() {
     for (let i = 0; i < images.length; i += 1) {
       formData.append('fileList', images[i].file);
     }
-    formData.append('personnel', courseInfo.personnel);
-    formData.append('description', courseInfo.description);
-    formData.append('budget', courseInfo.budget);
-    formData.append('title', courseInfo.title);
-    formData.append('courseRoute', courseInfo.courseRoute);
-    // tag들 추가
-    formData.append('transportation', courseInfo.transportation);
-    formData.append('local', courseInfo.local);
-    formData.append('theme', courseInfo.theme);
-    dispatch(courseInfoPost(formData));
+    // formData.append('personnel', courseInfo.personnel);
+    // formData.append('description', courseInfo.description);
+    // formData.append('budget', courseInfo.budget);
+    // formData.append('title', courseInfo.title);
+    // formData.append('courseRoute', courseInfo.courseRoute);
+    // // tag들 추가
+    // formData.append('transportation', courseInfo.transportation);
+    // formData.append('local', courseInfo.local);
+    // formData.append('theme', courseInfo.theme);
+    dispatch(courseImagePost(formData));
     // 2번 272~~~~304
-    // const arr = [];
-    // for (let i = 0; i < courseInfo.courseRoute.length; i += 1) {
-    //   const data = {
-    //     storeSeq: Number(courseInfo.courseRoute[i].id),
-    //     addressName: courseInfo.courseRoute[i].address_name,
-    //     categoryName: courseInfo.courseRoute[i].category_name,
-    //     phone: courseInfo.courseRoute[i].phone,
-    //     storeName: courseInfo.courseRoute[i].place_name,
-    //     placeUrl: courseInfo.courseRoute[i].place_url,
-    //     x: courseInfo.courseRoute[i].x,
-    //     y: courseInfo.courseRoute[i].y,
-    //   };
-    //   arr.push(data);
-    // }
-    // const data = {
-    //   personnel: courseInfo.personnel,
-    //   description: courseInfo.description,
-    //   budget: courseInfo.budget,
-    //   title: courseInfo.title,
-    //   courseRoute: arr,
-    //   transportation: courseInfo.transportation,
-    //   local: courseInfo.local,
-    //   theme: courseInfo.theme,
-    // };
+    const arr = [];
+    for (let i = 0; i < courseInfo.courseRoute.length; i += 1) {
+      const data = {
+        storeSeq: Number(courseInfo.courseRoute[i].id),
+        addressName: courseInfo.courseRoute[i].address_name,
+        categoryName: courseInfo.courseRoute[i].category_name,
+        phone: courseInfo.courseRoute[i].phone,
+        storeName: courseInfo.courseRoute[i].place_name,
+        placeUrl: courseInfo.courseRoute[i].place_url,
+        x: courseInfo.courseRoute[i].x,
+        y: courseInfo.courseRoute[i].y,
+      };
+      arr.push(data);
+    }
+    const data = {
+      personnel: courseInfo.personnel,
+      description: courseInfo.description,
+      budget: courseInfo.budget,
+      title: courseInfo.title,
+      courseRoute: arr,
+      transportation: courseInfo.transportation,
+      local: courseInfo.local,
+      theme: courseInfo.theme,
+    };
 
     // console.log(courseInfo);
     // // courseInfo.fileList = formData;
     // // 전부 formdata에 넣어서 보내주기
     // // dispatch(courseInfoPost(formData));
-    // dispatch(courseInfoPost(data));
+    dispatch(courseInfoPost(data));
     // dispatch(courseImagePost(formData));
     setCourseInfo(courseInfo);
     // console.log(data);
