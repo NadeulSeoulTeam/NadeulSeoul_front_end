806d9d7ce22225f44603a72df27b33abcfecd5cb
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-02-17 04:37:21 +0900

    Update : update develop

--- src/features/Main/MainPage/index.js
+++ src/features/Main/MainPage/index.js
@@ -56,7 +56,7 @@ function MainPage() {
   const handleOpen = () => {
     setOpen(!open);
   };
-  
+
   console.log(getLoginSuccess(), typeof getLoginSuccess());
 
   if (getLoginSuccess() === 'false') {
@@ -71,16 +71,16 @@ function MainPage() {
         });
     }, []);
   }
-  
+
   useEffect(() => {
     setLocalClicked(Array(localTag.length).fill(false));
     setThemeClicked(Array(themeTag.length).fill(false));
   }, []);
-  
+
   useEffect(() => {
     console.log(localClicked, themeClicked);
   }, [localClicked, themeClicked]);
-  
+
   useEffect(() => {
     console.log(localClicked);
     console.log(themeClicked);
@@ -98,7 +98,7 @@ function MainPage() {
       dispatch(LocalNThemeTagsSelected(data));
     }
   }, [clicked]);
-  
+
   // 태그 조건부 랜더링
   const tagSelectRender = (content) => {
     if (content === undefined) return <div />;
